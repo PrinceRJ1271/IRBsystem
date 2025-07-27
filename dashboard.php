02e33e575740a1e3490431f4c7ff1e44d74dbc48
Brijesh Chaube <BrijeshChaube@protonmail.com>
2025-07-28 01:36:37 +0800

    UpdateDashboardFormatting

--- dashboard.php
+++ dashboard.php
@@ -24,27 +24,36 @@ $level_id = $_SESSION['level_id'];
       <li><a href="#">Manage Users</a></li>
       <li><a href="#">Manage Security Levels</a></li>
       <li><a href="#">System Debugging</a></li>
+      <li><a href="forms/client_form.php">Register Client</a></li>
+      <li><a href="forms/irb_branch_form.php">Add IRB Branch</a></li>
+      <li><a href="forms/letter_type_form.php">Add Letter Type</a></li>
     </ul>
+
   <?php elseif ($level_id == 2): ?>
     <h3>Tax Manager Dashboard</h3>
     <ul>
       <li><a href="#">Register Staff</a></li>
-      <li><a href="#">Fill IRB Letter Types</a></li>
+      <li><a href="forms/client_form.php">Register Client</a></li>
+      <li><a href="forms/irb_branch_form.php">Add IRB Branch</a></li>
+      <li><a href="forms/letter_type_form.php">Add Letter Type</a></li>
       <li><a href="#">View Reports</a></li>
     </ul>
+
   <?php elseif ($level_id == 3): ?>
     <h3>Tax Senior Dashboard</h3>
     <ul>
-      <li><a href="#">Client Engagement Form</a></li>
+      <li><a href="forms/client_form.php">Client Engagement Form</a></li>
       <li><a href="#">Letter Received/Sent</a></li>
       <li><a href="#">Follow-up Forms</a></li>
     </ul>
+
   <?php elseif ($level_id == 4): ?>
     <h3>Admin Staff Dashboard</h3>
     <ul>
       <li><a href="#">Letter Delivery Form</a></li>
       <li><a href="#">Track Deliveries</a></li>
     </ul>
+
   <?php else: ?>
     <p>Invalid role. Please contact admin.</p>
   <?php endif; ?>
@@ -52,11 +61,3 @@ $level_id = $_SESSION['level_id'];
   <p><a href="logout.php">Logout</a></p>
 </body>
 </html>
-
-<?php if ($level_id == 2): ?>
-  <ul>
-    <li><a href="forms/client_form.php">Register Client</a></li>
-    <li><a href="forms/irb_branch_form.php">Add IRB Branch</a></li>
-    <li><a href="forms/letter_type_form.php">Add Letter Type</a></li>
-  </ul>
-<?php endif; ?>
