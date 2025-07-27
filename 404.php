24d03cf8bcfa22886913ea7335176206fbaf16f4
Brijesh Chaube <BrijeshChaube@protonmail.com>
2025-07-28 02:29:58 +0800

    AddingErrorHandling

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php
+http_response_code(404);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>404 - Page Not Found</title>
+</head>
+<body>
+  <h1>404</h1>
+  <p>Oops! The page you are looking for doesn't exist.</p>
+  <a href="/dashboard.php">Go to Dashboard</a>
+</body>
+</html>
