5c9c7ae766be519e97598a2e8bcb91d3de7b5cbd
Brijesh Chaube <BrijeshChaube@protonmail.com>
2025-07-29 20:03:43 +0800

    RevertToNicerDashboard

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,8 @@
-      </div> <!-- main-panel -->
-    </div> <!-- page-body-wrapper -->
-  </div> <!-- container-scroller -->
-
   <script src="assets/vendors/js/vendor.bundle.base.js"></script>
-  <script src="assets/vendors/chart.js/Chart.min.js"></script>
   <script src="assets/js/off-canvas.js"></script>
   <script src="assets/js/hoverable-collapse.js"></script>
   <script src="assets/js/template.js"></script>
+  <script src="assets/js/settings.js"></script>
   <script src="assets/js/todolist.js"></script>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,25 +1,12 @@
-<?php
-session_start();
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit();
-}
-$user_id = $_SESSION['user_id'];
-$level_id = $_SESSION['level_id'];
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>IRB System Dashboard</title>
-    <link rel="stylesheet" href="assets/vendors/feather/feather.css">
-    <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="assets/vendors/ti-icons/css/themify-icons.css">
-    <link rel="stylesheet" href="assets/vendors/typicons/typicons.css">
-    <link rel="stylesheet" href="assets/vendors/simple-line-icons/css/simple-line-icons.css">
-    <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
-    <link rel="stylesheet" href="assets/css/vertical-layout-light/style.css">
-    <link rel="shortcut icon" href="assets/images/favicon.png" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>IRB Letter Management System</title>
+  <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="shortcut icon" href="assets/images/favicon.png" />
 </head>
 <body>
-    <div class="container-scroller">
