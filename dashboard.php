564e0b754905bffeb3204f0cd81f87579249a0da
Brijesh Chaube <BrijeshChaube@protonmail.com>
2025-07-30 22:33:32 +0800

    RevertDashboardCosNicer

--- dashboard.php
+++ dashboard.php
@@ -39,25 +39,20 @@ $level_id = $_SESSION['level_id'];
         color: #343a40;
     }
     ul.list-arrow {
-        padding-left: 0;
+        padding-left: 20px;
         list-style-type: none;
     }
-    ul.list-arrow li {
-        margin-bottom: 12px;
+    ul.list-arrow li::before {
+        content: "➤";
+        color: #4e54c8;
+        margin-right: 8px;
     }
     ul.list-arrow li a {
-        display: flex;
-        align-items: center;
         text-decoration: none;
         color: #444;
         font-weight: 500;
     }
     ul.list-arrow li a:hover {
-        color: #4e54c8;
-    }
-    ul.list-arrow li i {
-        font-size: 18px;
-        margin-right: 10px;
         color: #4e54c8;
     }
   </style>
@@ -90,60 +85,60 @@ $level_id = $_SESSION['level_id'];
                   <?php if ($level_id == 1): ?>
                     <h4 class="card-title">Developer Dashboard</h4>
                     <ul class="list-arrow">
-                      <li><a href="#"><i class="mdi mdi-account-cog"></i>Manage Users</a></li>
-                      <li><a href="#"><i class="mdi mdi-shield-lock"></i>Manage Security Levels</a></li>
-                      <li><a href="#"><i class="mdi mdi-wrench"></i>System Debugging</a></li>
-                      <li><a href="forms/client_form.php"><i class="mdi mdi-account-plus"></i>Register Client</a></li>
-                      <li><a href="forms/irb_branch_form.php"><i class="mdi mdi-office-building"></i>Add IRB Branch</a></li>
-                      <li><a href="forms/letter_type_form.php"><i class="mdi mdi-file-document-multiple"></i>Add Letter Type</a></li>
-                      <li><a href="forms/letter_received_form.php"><i class="mdi mdi-email-receive"></i>IRB Letter Received Form</a></li>
-                      <li><a href="forms/letter_received_followup_form.php"><i class="mdi mdi-phone-incoming"></i>Follow-up Form (Received)</a></li>
-                      <li><a href="forms/letter_sent_form.php"><i class="mdi mdi-email-send"></i>IRB Letter Sent Form</a></li>
-                      <li><a href="forms/letter_sent_followup_form.php"><i class="mdi mdi-phone-outgoing"></i>Follow-up Form (Sent)</a></li>
-                      <li><a href="forms/letter_delivery_form.php"><i class="mdi mdi-truck-delivery"></i>Letter Delivery Form</a></li>
-                      <li><a href="search/quick_search.php"><i class="mdi mdi-magnify"></i>Quick Search</a></li>
-                      <li><a href="export/export_letters_pdf.php" target="_blank"><i class="mdi mdi-file-pdf-box"></i>Export Letters to PDF</a></li>
-                      <li><a href="export/export_letters_excel.php" target="_blank"><i class="mdi mdi-file-excel-box"></i>Export Letters to Excel</a></li>
+                      <li><a href="#">Manage Users</a></li>
+                      <li><a href="#">Manage Security Levels</a></li>
+                      <li><a href="#">System Debugging</a></li>
+                      <li><a href="forms/client_form.php">Register Client</a></li>
+                      <li><a href="forms/irb_branch_form.php">Add IRB Branch</a></li>
+                      <li><a href="forms/letter_type_form.php">Add Letter Type</a></li>
+                      <li><a href="forms/letter_received_form.php">IRB Letter Received Form</a></li>
+                      <li><a href="forms/letter_received_followup_form.php">Follow-up Form (Received)</a></li>
+                      <li><a href="forms/letter_sent_form.php">IRB Letter Sent Form</a></li>
+                      <li><a href="forms/letter_sent_followup_form.php">Follow-up Form (Sent)</a></li>
+                      <li><a href="forms/letter_delivery_form.php">Letter Delivery Form</a></li>
+                      <li><a href="search/quick_search.php">Quick Search</a></li>
+                      <li><a href="export/export_letters_pdf.php" target="_blank">Export Letters to PDF</a></li>
+                      <li><a href="export/export_letters_excel.php" target="_blank">Export Letters to Excel</a></li>
                     </ul>
 
                   <?php elseif ($level_id == 2): ?>
                     <h4 class="card-title">Tax Manager Dashboard</h4>
                     <ul class="list-arrow">
-                      <li><a href="#"><i class="mdi mdi-account-plus"></i>Register Staff</a></li>
-                      <li><a href="forms/client_form.php"><i class="mdi mdi-account-plus"></i>Register Client</a></li>
-                      <li><a href="forms/irb_branch_form.php"><i class="mdi mdi-office-building"></i>Add IRB Branch</a></li>
-                      <li><a href="forms/letter_type_form.php"><i class="mdi mdi-file-document-multiple"></i>Add Letter Type</a></li>
-                      <li><a href="forms/letter_received_form.php"><i class="mdi mdi-email-receive"></i>IRB Letter Received Form</a></li>
-                      <li><a href="forms/letter_received_followup_form.php"><i class="mdi mdi-phone-incoming"></i>Follow-up Form (Received)</a></li>
-                      <li><a href="forms/letter_sent_form.php"><i class="mdi mdi-email-send"></i>IRB Letter Sent Form</a></li>
-                      <li><a href="forms/letter_sent_followup_form.php"><i class="mdi mdi-phone-outgoing"></i>Follow-up Form (Sent)</a></li>
-                      <li><a href="#"><i class="mdi mdi-chart-box-outline"></i>View Reports</a></li>
-                      <li><a href="search/quick_search.php"><i class="mdi mdi-magnify"></i>Quick Search</a></li>
-                      <li><a href="export/export_letters_pdf.php" target="_blank"><i class="mdi mdi-file-pdf-box"></i>Export Letters to PDF</a></li>
-                      <li><a href="export/export_letters_excel.php" target="_blank"><i class="mdi mdi-file-excel-box"></i>Export Letters to Excel</a></li>
+                      <li><a href="#">Register Staff</a></li>
+                      <li><a href="forms/client_form.php">Register Client</a></li>
+                      <li><a href="forms/irb_branch_form.php">Add IRB Branch</a></li>
+                      <li><a href="forms/letter_type_form.php">Add Letter Type</a></li>
+                      <li><a href="forms/letter_received_form.php">IRB Letter Received Form</a></li>
+                      <li><a href="forms/letter_received_followup_form.php">Follow-up Form (Received)</a></li>
+                      <li><a href="forms/letter_sent_form.php">IRB Letter Sent Form</a></li>
+                      <li><a href="forms/letter_sent_followup_form.php">Follow-up Form (Sent)</a></li>
+                      <li><a href="#">View Reports</a></li>
+                      <li><a href="search/quick_search.php">Quick Search</a></li>
+                      <li><a href="export/export_letters_pdf.php" target="_blank">Export Letters to PDF</a></li>
+                      <li><a href="export/export_letters_excel.php" target="_blank">Export Letters to Excel</a></li>
                     </ul>
 
                   <?php elseif ($level_id == 3): ?>
                     <h4 class="card-title">Tax Senior Dashboard</h4>
                     <ul class="list-arrow">
-                      <li><a href="forms/client_form.php"><i class="mdi mdi-account-box-outline"></i>Client Engagement Form</a></li>
-                      <li><a href="forms/letter_received_form.php"><i class="mdi mdi-email-receive"></i>IRB Letter Received Form</a></li>
-                      <li><a href="forms/letter_received_followup_form.php"><i class="mdi mdi-phone-incoming"></i>Follow-up Form (Received)</a></li>
-                      <li><a href="forms/letter_sent_form.php"><i class="mdi mdi-email-send"></i>IRB Letter Sent Form</a></li>
-                      <li><a href="forms/letter_sent_followup_form.php"><i class="mdi mdi-phone-outgoing"></i>Follow-up Form (Sent)</a></li>
-                      <li><a href="search/quick_search.php"><i class="mdi mdi-magnify"></i>Quick Search</a></li>
-                      <li><a href="export/export_letters_pdf.php" target="_blank"><i class="mdi mdi-file-pdf-box"></i>Export Letters to PDF</a></li>
-                      <li><a href="export/export_letters_excel.php" target="_blank"><i class="mdi mdi-file-excel-box"></i>Export Letters to Excel</a></li>
+                      <li><a href="forms/client_form.php">Client Engagement Form</a></li>
+                      <li><a href="forms/letter_received_form.php">IRB Letter Received Form</a></li>
+                      <li><a href="forms/letter_received_followup_form.php">Follow-up Form (Received)</a></li>
+                      <li><a href="forms/letter_sent_form.php">IRB Letter Sent Form</a></li>
+                      <li><a href="forms/letter_sent_followup_form.php">Follow-up Form (Sent)</a></li>
+                      <li><a href="search/quick_search.php">Quick Search</a></li>
+                      <li><a href="export/export_letters_pdf.php" target="_blank">Export Letters to PDF</a></li>
+                      <li><a href="export/export_letters_excel.php" target="_blank">Export Letters to Excel</a></li>
                     </ul>
 
                   <?php elseif ($level_id == 4): ?>
                     <h4 class="card-title">Admin Staff Dashboard</h4>
                     <ul class="list-arrow">
-                      <li><a href="forms/letter_delivery_form.php"><i class="mdi mdi-truck-fast"></i>Letter Delivery Form</a></li>
-                      <li><a href="#"><i class="mdi mdi-map-marker-path"></i>Track Deliveries</a></li>
-                      <li><a href="search/quick_search.php"><i class="mdi mdi-magnify"></i>Quick Search</a></li>
-                      <li><a href="export/export_letters_pdf.php" target="_blank"><i class="mdi mdi-file-pdf-box"></i>Export Letters to PDF</a></li>
-                      <li><a href="export/export_letters_excel.php" target="_blank"><i class="mdi mdi-file-excel-box"></i>Export Letters to Excel</a></li>
+                      <li><a href="forms/letter_delivery_form.php">Letter Delivery Form</a></li>
+                      <li><a href="#">Track Deliveries</a></li>
+                      <li><a href="search/quick_search.php">Quick Search</a></li>
+                      <li><a href="export/export_letters_pdf.php" target="_blank">Export Letters to PDF</a></li>
+                      <li><a href="export/export_letters_excel.php" target="_blank">Export Letters to Excel</a></li>
                     </ul>
 
                   <?php else: ?>
