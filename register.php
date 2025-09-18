ef5f0081d90ffbf7bf74342858f696c6550087a7
Brijesh Chaube <BrijeshChaube@protonmail.com>
2025-09-18 15:39:01 +0800

    Update register.php

--- register.php
+++ register.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/config/db.php';
 require_once __DIR__ . '/includes/auth.php';
 
 // Allow ONLY Developer (1)
-check_access([1]);
+check_access([1, 4]);
 
 $error   = "";
 $success = "";
